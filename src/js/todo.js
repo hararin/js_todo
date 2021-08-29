d7445302d8f17e1b2d28567293da9c0bf5e08bd8
hararin <69122070+hararin@users.noreply.github.com>
2021-08-30 01:48:35 +0900

    todoリスト完成

--- src/js/todo.js
+++ src/js/todo.js
@@ -9,13 +9,23 @@ const removeTask = removeButton => {
   addTaskTarget.removeChild(targetTask);
 };
 
+const completeTask = completeButton => {
+  const targetTask = completeButton.closest('li');
+  targetTask.classList.add('isCompleted');
+  targetTask.removeChild(completeButton);
+}
+
 const addTask = task => {
   const listItem = document.createElement('li');
   const removeButton = document.createElement('button');
+  const completeButton = document.createElement('button');
   removeButton.innerText = '削除';
+  completeButton.innerText = '完了';
   removeButton.addEventListener('click', () => removeTask(removeButton));
+  completeButton.addEventListener('click', () => completeTask(completeButton));
   listItem.innerText = task;
   listItem.append(removeButton);
+  listItem.append(completeButton);
   addTaskTarget.appendChild(listItem);
 };
 
