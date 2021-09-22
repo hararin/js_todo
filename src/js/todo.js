cd64581defbaab5e7aad385b6e4d95c0a68c14ef
hararin <69122070+hararin@users.noreply.github.com>
2021-09-23 01:19:55 +0900

    doneリストの削除機能を修正

--- src/js/todo.js
+++ src/js/todo.js
@@ -3,27 +3,34 @@
 const addTaskTrigger = document.getElementsByClassName('js-addTask-trigger')[0];
 const addTaskTarget = document.getElementsByClassName('js-addTask-target')[0];
 const addTaskValue = document.getElementsByClassName('js-addTask-value')[0];
+const finishedTaskTarget = document.getElementsByClassName('js-finishedTask-target')[0];
 
 const removeTask = removeButton => {
   const targetTask = removeButton.closest('li');
   addTaskTarget.removeChild(targetTask);
 };
 
-const completeTask = completeButton => {
+const subRemoveTask = removeButton => {
+  const targetTask = removeButton.closest('li');
+  finishedTaskTarget.removeChild(targetTask);
+};
+
+const completeTask = (completeButton, removeButton) => {
   const targetTask = completeButton.closest('li');
-  targetTask.classList.add('isCompleted');
   targetTask.removeChild(completeButton);
   const returnButton = document.createElement('button');
   returnButton.innerText = '未完了';
-  returnButton.addEventListener('click', () => returnTask(returnButton, completeButton));
-  targetTask.appendChild(returnButton);
+  returnButton.addEventListener('click', () => returnTask(returnButton, completeButton, removeButton));
+  removeButton.addEventListener('click', () => subRemoveTask(removeButton));
+  targetTask.append(returnButton);
+  finishedTaskTarget.appendChild(targetTask);
 }
 
-const returnTask = (returnButton, completeButton) => {
+const returnTask = (returnButton, completeButton, removeButton) => {
   const targetTask = returnButton.closest('li');
-  targetTask.classList.remove('isCompleted');
   targetTask.removeChild(returnButton);
   targetTask.appendChild(completeButton);
+  addTaskTarget.appendChild(targetTask);
 }
 
 const addTask = task => {
@@ -33,12 +40,13 @@ const addTask = task => {
   removeButton.innerText = '削除';
   completeButton.innerText = '完了';
   removeButton.addEventListener('click', () => removeTask(removeButton));
-  completeButton.addEventListener('click', () => completeTask(completeButton));
+  completeButton.addEventListener('click', () => completeTask(completeButton, removeButton));
   listItem.innerText = task;
   listItem.append(removeButton);
   listItem.append(completeButton);
   addTaskTarget.appendChild(listItem);
 };
+
 
 addTaskTrigger.addEventListener('click', event => {
   const task = addTaskValue.value;
